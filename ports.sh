edf8e725aca4662a6548489f12291efeaa3b3574
Kirsten Hunter <77410784+synedra@users.noreply.github.com>
2024-09-10 09:04:32 -0700

    Update ports.sh

--- ports.sh
+++ ports.sh
@@ -5,4 +5,5 @@ export FUSIONAUTH_URL=`gp url 9011 | perl -pe 's/https/http/g'`
 export FUSIONAUTH_9012_URL=`gp url 9012 | perl -pe 's/https/http/g'`
 
 perl -pi -e 's/REACT_APP_FUSIONAUTH/$ENV{FUSIONAUTH_URL}/g' complete-application/src/main.tsx kickstart/kickstart.json serverup.sh
+perl -pi -e 's/FUSIONAUTH_9012_URL/$ENV{FUSIONAUTH_9012_URL}/g' complete-application/src/main.tsx kickstart/kickstart.json serverup.sh
 perl -pi -e 's/REACT_APP_REDIRECT/$ENV{REDIRECT_URL}/g' complete-application/src/main.tsx kickstart/kickstart.json serverup.sh
